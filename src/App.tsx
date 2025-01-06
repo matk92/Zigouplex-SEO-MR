daeb970680b199c88d01f06691e822599dfa1226
Mathieu K <mathieu.keca@gmail.com>
2025-01-06 11:10:06 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,13 @@ import BlogPostBienfaits from './pages/BlogPostBienfaits'
 import BlogPostBoosterEnergie from './pages/BlogPostBoosterEnergie'
 import BlogPostZigouplexVsTraditional from './pages/BlogPostZigouplexVsTraditional'
 import BlogLesBienfaitsDeZigouplexParDrMarlega from './pages/BlogLesBienfaitsDeZigouplexParDrMarlega'
-import LesBienfaitsDeLaNatureZigouplex from './pages/LesBienfaitsDeLaNatureZigouplex';
+import LesBienfaitsDeLaNatureZigouplex from './pages/LesBienfaitsDeLaNatureZigouplex'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ForgotPassword from './pages/ForgotPassword'
 import NotFound from './pages/NotFound'
 import BlogPostBakeryPartnership from './pages/BlogPostBakeryPartnership'
+import BlogPostNewYear2025 from './pages/BlogPostNewYear2025'
 
 function App() {
   return (
@@ -44,6 +45,7 @@ function App() {
               <Route path="/blog/les-bienfaits-de-zigouplex-par-dr-marlega" element={<BlogLesBienfaitsDeZigouplexParDrMarlega />} />
               <Route path="/blog/Les-Bienfaits-de-la-Nature-Zigouplex-Blog" element={<LesBienfaitsDeLaNatureZigouplex />} />
               <Route path="/blog/bakery-partnership" element={<BlogPostBakeryPartnership />} />
+              <Route path="/blog/nouvelle-annee-2025-avec-zigouplex" element={<BlogPostNewYear2025 />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
